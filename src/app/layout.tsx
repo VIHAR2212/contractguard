3f7477784436e3d8c5232974bed86c61e9b8ce6d
Vihar Makwana <makwanavihar22@gmail.com>
2026-07-04 15:23:46 +0530

    Change app icon URL to local logo file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
   ],
   authors: [{ name: "ContractGuard" }],
   icons: {
-    icon: "https://z-cdn.chatglm.cn/z-ai/static/logo.svg",
+    icon: "/logo.png",
   },
   openGraph: {
     title: "ContractGuard",
